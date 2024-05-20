597ffaf4b14ab9c1b1f0a3b6971fc07a135688a7
Erica Marigold <hi@devcomp.xyz>
2024-05-20 20:05:06 +0530

    feat: toggle system & improved locking mechanism

--- src/main/java/xyz/devcomp/elytralock/ElytraLock.java
+++ src/main/java/xyz/devcomp/elytralock/ElytraLock.java
@@ -1,21 +1,46 @@
 package xyz.devcomp.elytralock;
 
+import org.lwjgl.glfw.GLFW;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.impl.networking.client.ClientPlayNetworkAddon;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
+import net.minecraft.network.packet.c2s.play.SlotChangedStateC2SPacket;
 
 public class ElytraLock implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Elytra Lock");
-	
+	private static KeyBinding lockKeybind;
+	private static boolean locked = false;
+	private static MinecraftClient client;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		lockKeybind = KeyBindingHelper.registerKeyBinding(
+				new KeyBinding("key.elytralock.lock", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J, "category.elytralock"));
+		client = MinecraftClient.getInstance();
+
 		LOGGER.info("Elytra lock initializing!");
 	}
 
 	public static boolean isLocked() {
-		// TODO: Load from config
-		return true;
+		if (lockKeybind.wasPressed()) {
+			locked = !locked;
+		}
+
+		client.inGameHud.getChatHud().addMessage(Text.translatable("elytralock.chat.lockedMessage"));
+
+		return locked;
 	}
 }
