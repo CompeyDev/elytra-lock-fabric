813717068b23a71ca9d2e1c5f4362d5403416d12
Erica Marigold <hi@devcomp.xyz>
2024-05-20 20:06:46 +0530

    fix: move translations into correct resources dir

--- src/main/java/xyz/devcomp/elytralock/ElytraLock.java
+++ src/main/java/xyz/devcomp/elytralock/ElytraLock.java
@@ -7,17 +7,10 @@ import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.impl.networking.client.ClientPlayNetworkAddon;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.network.message.MessageType;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
-import net.minecraft.network.packet.c2s.play.SlotChangedStateC2SPacket;
 
 public class ElytraLock implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Elytra Lock");
