ccd3c00ff5a6770db3bbcfe13dd5597131b6faf9
Erica Marigold <hi@devcomp.xyz>
2024-08-19 00:04:43 +0530

    chore: remove accidentally committed remapped sources

--- remappedSrc/xyz/devcomp/elytralock/config/ConfigUtil.java
+++
@@ -1,9 +0,0 @@
-package xyz.devcomp.elytralock.config;
-
-import xyz.devcomp.elytralock.ElytraLock;
-
-public class ConfigUtil {
-    public static boolean isYaclLoaded() {
-        return ElytraLock.LOADER.isModLoaded("yet_another_config_lib_v3");
-    }
-}
